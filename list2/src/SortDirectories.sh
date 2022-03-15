8c59aed633aaee9bc9f600c70775a539e7762794
Jakub-Bednarek <BlackFlage10@gmail.com>
2022-03-15 01:17:35 +0100

    SortDirectories script fix

--- list2/src/SortDirectories.sh
+++ list2/src/SortDirectories.sh
@@ -1,6 +1,7 @@
-$path = $args[0]
-$nfiles_to_show = $args[1]
-$files = (Get-ChildItem -path $path -Recurse | Select-Object Name, Size -First $nfiles_to_show | Sort-Object -Property Size -Descending)
+$pattern = $args[0]
+$path = $args[1]
+$nfiles_to_show = $args[2]
+$files = (Get-ChildItem -path $path -Recurse | Sort-Object -Property Size -Descending) | Where-Object { $_.name -Match $pattern } | Select-Object Name, Size -First $nfiles_to_show 
 $result = @()
 
 echo $files
