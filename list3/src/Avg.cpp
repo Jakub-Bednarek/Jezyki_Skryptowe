2898d7c98d66f2bf0b8624fe2829091a7fb75bd5
Jakub-Bednarek <BlackFlage10@gmail.com>
2022-03-20 23:03:24 +0100

    Pulled is_number to Uitls.hpp, added Avg target

--- list3/src/Avg.cpp
+++ list3/src/Avg.cpp
@@ -5,15 +5,17 @@
 int main(int argc, char* argv[])
 {
     double sum = 0.0;
+    int n_numbers = 0;
     std::string input;
     while(std::cin >> input)
     {
         if(is_number(input))
         {
             sum += std::stod(input);
+            n_numbers++;
         }
     }
 
-    std::cout << sum << '\n';
+    std::cout << sum / n_numbers << '\n';
     return 0;
 }
