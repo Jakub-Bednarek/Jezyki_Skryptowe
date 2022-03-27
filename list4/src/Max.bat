4a55679ae5ec707c6ccd0b078618817654236713
Jakub-Bednarek <blackflage10@gmail.com>
2022-03-27 17:59:43 +0200

    Added support for negative number in Max script

--- list4/src/Max.bat
+++ list4/src/Max.bat
@@ -1,14 +1,18 @@
 @echo off
-set highest_value=
+set highest_value=""
 
 setlocal enabledelayedexpansion
 for %%a in (%*) do (
     set current_val=%%a
     echo !current_val! | findstr /r "[-][1-9][0-9]* [1-9][0-9]*">nul && (
-        if !current_val! gtr !highest_value! (
+        if !highest_value! equ "" (
             set /a highest_value=current_val
+        ) else (
+            if !current_val! gtr !highest_value! (
+                set /a highest_value=current_val
+            )
         )
     )
 )
 
-echo %highest_value%
+if %highest_value% neq "" echo %highest_value%
