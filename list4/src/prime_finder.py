c886897f936a682e0468099f432d94b52ac735f6
Jakub-Bednarek <BlackFlage10@gmail.com>
2022-03-27 22:52:17 +0200

    Renamed script, added prime_finder

---
+++ list4/src/prime_finder.py
@@ -0,0 +1,32 @@
+import sys
+
+def is_prime(number):
+    if number <= 1: return False
+    if number <= 3: return True
+    
+    if number % 2 == 0 or number % 3 == 0: return False
+    
+    i = 5
+    while i * i < number:
+        if number % i == 0 or number % (i + 2) == 0: return False
+        i += 6
+    
+    return True
+
+def get_next_prime(number):
+    next_number = number + 1
+    while not is_prime(next_number):
+        next_number += 1
+        
+    return next_number
+
+def main():
+    arguments = sys.argv[1:]
+    
+    for arg in arguments:
+        next_prime = get_next_prime(int(arg))
+        print(f"{arg} -> {next_prime}")
+        
+    
+if __name__ == "__main__":
+    main()
