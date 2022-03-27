6b407d8b5ba53a09b58348982891727569e0a60d
Jakub-Bednarek <blackflage10@gmail.com>
2022-03-27 18:34:13 +0200

    Renamed Max script, added srednia

---
+++ list4/src/srednia.bat
@@ -0,0 +1,20 @@
+@echo off
+
+set /a sum=0
+set /a count=0
+
+setlocal enabledelayedexpansion
+for %%a in (%*) do (
+    set current_val=%%a
+    echo !current_val! | findstr /r "[-][1-9][0-9]* [1-9][0-9]* 0">nul && (
+        set /a sum=sum+!current_val!
+        set /a count=count+1
+    )
+)
+
+if %count% gtr 0 (
+    set /a sum=sum*1000
+    set /a count=count
+    set /a avg=!sum! / !count!
+    echo !avg:~0,-3!.!avg:~-3!
+)
