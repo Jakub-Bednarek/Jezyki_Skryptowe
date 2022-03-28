bfef0053bf5e751fe44f441b361229592e784550
Jakub-Bednarek <blackflage10@gmail.com>
2022-03-28 22:01:32 +0200

    pi_checker imporvements

--- list4/src/pi_checker.py
+++ list4/src/pi_checker.py
@@ -7,9 +7,8 @@ def check_pi_precision(precision):
     n = 1.0
     sum = 1.0
     while current_pi_index <= precision:
-        val = (2 * n) * (2 * n) / ((2 * n - 1) * (2 * n + 1))
+        sum *= (2 * n) * (2 * n) / ((2 * n - 1) * (2 * n + 1))
         n += 1
-        sum *= val
         sum_str = str(sum * 2)
         if sum_str[current_pi_index] == pi_str[current_pi_index]:
             current_pi_index += 1
