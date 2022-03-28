5c07ddf12988e7716582e5d287c309b05f609872
Jakub Bednarek <260387@student.pwr.edu.pl>
2022-03-28 19:49:47 +0200

    prime_finder arguments checking

--- list4/src/prime_finder.py
+++ list4/src/prime_finder.py
@@ -3,7 +3,6 @@ import sys
 def is_prime(number):
     if number <= 1: return False
     if number <= 3: return True
-    
     if number % 2 == 0 or number % 3 == 0: return False
     
     i = 5
@@ -21,12 +20,12 @@ def get_next_prime(number):
     return next_number
 
 def main():
-    arguments = sys.argv[1:]
-    
-    for arg in arguments:
-        next_prime = get_next_prime(int(arg))
-        print(f"{arg} -> {next_prime}")
-        
+    for arg in sys.argv[1:]:
+        try:
+            next_prime = get_next_prime(int(arg))
+            print(f"{arg} -> {next_prime}")
+        except (ValueError, IndexError):
+            pass
     
 if __name__ == "__main__":
     main()
