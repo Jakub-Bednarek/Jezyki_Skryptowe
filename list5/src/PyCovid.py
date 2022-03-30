05bd5e15c3b3ddf81c3db82352f65a3ff6f7a80b
Jakub-Bednarek <BlackFlage10@gmail.com>
2022-03-30 20:58:03 +0200

    PyCovid script

---
+++ list5/src/PyCovid.py
@@ -0,0 +1,60 @@
+from argparse import ArgumentError
+from ast import parse
+from multiprocessing.sharedctypes import Value
+import sys
+
+from click import argument
+
+DEFAULT_COVID_FILENAME = "Covid.txt"
+DEFAULT_COUNTRY = "Poland"
+DEFAULT_MONTH = "3"
+DEFAULT_COUNTRY_INDEX = 6
+DEFAULT_MONTH_INDEX = 2
+DEFAULT_CASES_INDEX = 4
+EXPECTED_TOKENS_IN_LINE = 12
+EXPECTED_ARGUMENTS_SIZE = 3
+
+def extract_cases(line: str, month: str, country: str) -> int:
+    tokens = line.split()
+    
+    if len(tokens) != EXPECTED_TOKENS_IN_LINE:
+        return 0
+    
+    cases = 0
+    if tokens[DEFAULT_MONTH_INDEX] == month and tokens[DEFAULT_COUNTRY_INDEX] == country:
+        try:
+            cases = int(tokens[DEFAULT_CASES_INDEX])
+        except ValueError:
+            pass
+    
+    return cases
+
+def get_all_cases(month: str, country: str):
+    total_cases = 0
+    with open(DEFAULT_COVID_FILENAME) as f:
+        all_lines = f.readlines()
+        
+        for line in all_lines:
+            total_cases += extract_cases(line, month, country)
+            
+    return total_cases
+            
+
+def parse_arguments():
+    arguments = sys.argv
+    if len(arguments) == 1:
+        return DEFAULT_MONTH, DEFAULT_COUNTRY
+    
+    if arguments[1].isnumeric():
+        return arguments[1], arguments[2]
+    else:
+        return arguments[2], arguments[1]
+
+def main():
+    month, country = parse_arguments()
+    print(f"{month}, {country}")
+    print(get_all_cases(month, country))
+    
+
+if __name__ == "__main__":
+    main()
