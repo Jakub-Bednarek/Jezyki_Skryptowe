4dba382bd0f6df4f793cd1d8756244baeb6fc785
Jakub-Bednarek <BlackFlage10@gmail.com>
2022-04-10 23:04:18 +0200

    Changed name of country script

---
+++ list6/src/sum_for_country.py
@@ -0,0 +1,55 @@
+import sys
+from timeit import default_timer as timer
+from covid_parser import parse_data_from_file
+
+All_dates, By_date, By_country = parse_data_from_file()
+DEFAULT_COUNTRY = "Poland"
+
+
+def for_country_a(country):
+    start = timer()
+    deaths, cases = 0, 0
+    for record in All_dates:
+        if record[0] == country:
+            deaths += record[4]
+            cases += record[5]
+            
+    return (deaths, cases)
+
+
+def for_country_d(country):
+    start = timer()
+    deaths, cases = 0, 0
+    for key, value in By_date.items():
+        for record in value:
+            if record[0] == country:
+                deaths += record[1]
+                cases += record[2]
+    return (deaths, cases)
+
+
+def for_country_c(country):
+    start = timer()
+    deaths, cases = 0, 0
+    for key, value in By_country.items():
+        if key == country:
+            for record in value:
+                deaths += record[3]
+                cases += record[4]
+    return (deaths, cases)
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("No argument provided for country!")
+        country = DEFAULT_COUNTRY
+    else:
+        country = sys.argv[1]
+    
+    print(for_country_a(country))
+    print(for_country_d(country))
+    print(for_country_c(country))
+
+
+if __name__ == "__main__":
+    main()
