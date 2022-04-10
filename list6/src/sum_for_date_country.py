6ad7e8a021628bc0b09e3477dd9718e77f912bfa
Jakub-Bednarek <BlackFlage10@gmail.com>
2022-04-10 23:09:10 +0200

    Added sum_for_date_country script (task 4)

--- list6/src/sum_for_date_country.py
+++ list6/src/sum_for_date_country.py
@@ -4,38 +4,43 @@ from covid_parser import parse_data_from_file
 
 All_dates, By_date, By_country = parse_data_from_file()
 DEFAULT_COUNTRY = "Poland"
+DEFAULT_YEAR = 2020
+DEFAULT_MONTH = 1
+DEFAULT_DAY = 1
 
 
-def for_country_a(country):
+def for_country_a(year, month, day, country):
     start = timer()
     deaths, cases = 0, 0
     for record in All_dates:
-        if record[0] == country:
+        if record[0] == country and record[1] == year and record[2] == month and record[3] == day:
             deaths += record[4]
             cases += record[5]
             
     return (deaths, cases)
 
 
-def for_country_d(country):
+def for_country_d(year, month, day, country):
     start = timer()
     deaths, cases = 0, 0
     for key, value in By_date.items():
-        for record in value:
-            if record[0] == country:
-                deaths += record[1]
-                cases += record[2]
+        if key == (year, month, day):
+            for record in value:
+                if record[0] == country:
+                    deaths += record[1]
+                    cases += record[2]
     return (deaths, cases)
 
 
-def for_country_c(country):
+def for_country_c(year, month, day, country):
     start = timer()
     deaths, cases = 0, 0
     for key, value in By_country.items():
-        if key == country:
+        if key == country: 
             for record in value:
-                deaths += record[3]
-                cases += record[4]
+                if(record[0], record[1], record[2]) == (year, month, day):
+                    deaths += record[3]
+                    cases += record[4]
     return (deaths, cases)
 
 
@@ -46,9 +51,9 @@ def main():
     else:
         country = sys.argv[1]
     
-    print(for_country_a(country))
-    print(for_country_d(country))
-    print(for_country_c(country))
+    print(for_country_a(2020, 10, 10, country))
+    print(for_country_d(2020, 10, 10, country))
+    print(for_country_c(2020, 10, 10, country))
 
 
 if __name__ == "__main__":
