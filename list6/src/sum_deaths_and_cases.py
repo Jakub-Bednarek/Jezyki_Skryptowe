44b5928937c3a548cb569d93eeb55c7aef831aa2
Jakub-Bednarek <BlackFlage10@gmail.com>
2022-04-10 22:54:10 +0200

    Handling command line arguments

--- list6/src/sum_deaths_and_cases.py
+++ list6/src/sum_deaths_and_cases.py
@@ -3,7 +3,9 @@ from timeit import default_timer as timer
 from covid_parser import parse_data_from_file
 
 All_dates, By_date, By_country = parse_data_from_file()
-
+DEFAULT_YEAR = 2020
+DEFAULT_MONTH = 1
+DEFAULT_DAY = 1
 
 def for_date_a(year, month, day):
     start = timer()
@@ -47,9 +49,17 @@ def for_date_c(year, month, day):
         
 
 def main():
-    print(for_date_a(2020, 3, 21))
-    print(for_date_d(2020, 3, 21))
-    print(for_date_c(2020, 3, 21))
+    args = sys.argv
+    
+    try:
+        year, month, day = int(args[1]), int(args[2]), int(args[3])
+    except:
+        print("Invalid arguments!")
+        year, month, day = DEFAULT_YEAR, DEFAULT_MONTH, DEFAULT_DAY
+    
+    print(for_date_a(year, month, day))
+    print(for_date_d(year, month, day))
+    print(for_date_c(year, month, day))
     
     
 if __name__ == "__main__":
