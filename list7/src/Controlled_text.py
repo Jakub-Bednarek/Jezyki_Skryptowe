9b3ec3851e79498749ba87678180cc1ac5cbd4eb
Jakub-Bednarek <BlackFlage10@gmail.com>
2022-04-25 20:37:25 +0200

    Added Controlled_text class

---
+++ list7/src/Controlled_text.py
@@ -0,0 +1,24 @@
+class Controlled_text:
+    def __init__(self, text):
+        self._text = text
+
+    @property
+    def text(self):
+        return self._text
+
+    @text.getter
+    def text(self):
+        return self._text
+
+    @text.setter
+    def text(self, new_text):
+        if not new_text.isprintable() or " " in new_text:
+            raise ValueError("Invalid argument provided!")
+        else:
+            self._text = new_text
+
+    def __str__(self):
+        return self._text
+
+    def __lt__(self, other):
+        return self._text < other._text
