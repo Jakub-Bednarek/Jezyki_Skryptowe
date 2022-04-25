8db6f0ccb5674b782ce9fca295e9ac488773e900
Jakub-Bednarek <BlackFlage10@gmail.com>
2022-04-25 20:37:50 +0200

    Last_name class without checks

---
+++ list7/src/Last_name.py
@@ -0,0 +1,27 @@
+from Controlled_text import Controlled_text
+
+
+class Last_name(Controlled_text):
+    def __init__(self, last_name):
+        self.lname = last_name
+
+    @property
+    def lname(self):
+        return self._text
+
+    @lname.getter
+    def lname(self):
+        return self._text
+
+    @lname.setter
+    def lname(self, new_lname):
+        self.__check_last_name(new_lname)
+        self._text = new_lname
+
+    def __check_last_name(self, last_name):
+        if not last_name.istitle():
+            raise ValueError("Invalid argument for last name!")
+
+
+l1 = Last_name("Bet-Onarek")
+print(l1)
